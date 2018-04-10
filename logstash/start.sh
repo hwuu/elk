cbfefccb3225cd57e59f57b0ea61df71b3ec268b
Hao Wu <wuh@microsoft.com>
2018-04-09 21:09:09 -0400

    Refine configurations of Logstash.

--- logstash/start.sh
+++ logstash/start.sh
@@ -1,3 +1,3 @@
 docker build -t my-logstash .
-docker run -ti --rm -p 5140 --net=host -v /var/log:/var/log --name my-logstash my-logstash
+docker run -d -p 5140 --net=host -v /var/log:/var/log --name my-logstash my-logstash
 
